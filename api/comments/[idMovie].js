9bce10d2c083337cd8429da3d1bb7faae8a8ab8f
Santo33310 <121873144+Santo33310@users.noreply.github.com>
2023-04-29 13:47:06 +0200

    Add files via upload

---
+++ api/comments/[idMovie].js
@@ -0,0 +1,15 @@
+import clientPromise from "../../../lib/mongodb";
+import { ObjectId } from "mongodb";
+
+export default async function handler (req, res) {
+
+    const idMovie = req.query.idMovie;
+
+    console.log('idMovie',idMovie) ;
+
+    const client = await clientPromise;
+    const db = client.db("sample_mflix");
+
+    const dbComments = await db.collection("comments").find({movie_id: new ObjectId(idMovie)}).toArray();
+    res.json({ status: 200, data: {comments: dbComments}});
+}
